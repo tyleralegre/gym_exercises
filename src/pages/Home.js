0266b7e5447df61806019696c68771b2e22d7e8d
Tyler <109047657+tyleralegre@users.noreply.github.com>
2023-03-08 16:38:29 -0800

    files

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import {Box} from '@mui/material';
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
+import BodyPart from '../components/BodyPart';
+
+const Home = () => {
+  const [bodyPart,setBodyPart] = useState('all');
+  const [exercises, setExercises] = useState([]);
+  return (
+  <Box>
+    <HeroBanner/>
+    <SearchExercises setExercises={setExercises}
+    bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+    <Exercises
+    exercises={exercises}
+    setExercises={setExercises}
+    bodyPart={bodyPart} />
+    </Box>
+  )
+}
+
+export default Home
